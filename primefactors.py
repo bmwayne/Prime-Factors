a99f9abf8a176332a89cfcd4838411fb285bee3d
Bruce Wayne <rohitvarmachitti@gmail.com>
2021-01-20 21:44:46 -0600

    Changes

---
+++ primefactors.py
@@ -0,0 +1,13 @@
+factors = []
+
+
+def isPrime():
+    n = int(input("enter a number:"))
+
+    for i in range(1,n):
+        if n%i == 0:
+            factors.append(i)
+            n = n/i
+    return factors
+
+print(isPrime())
